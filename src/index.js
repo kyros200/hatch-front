c2073837e188ec994ef5afb1f4709d5d42000227
rafael najjar <rafaelnajjar@gmail.com>
2022-01-11 01:18:58 -0300

    removed unused libs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import Landing from './components/Landing';
 import Error from './components/Error';
 import PageLayout from './components/PageLayout';
@@ -40,8 +39,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
